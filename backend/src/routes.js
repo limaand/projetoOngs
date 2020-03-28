9ff4e399d678bfda55ffb96ba157e5c051ce24b5
limaand <limaand@gmail.com>
2020-03-28 17:19:12 -0300

    first commit

---
+++ backend/src/routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const routes = express.Router();
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.post('/sessions', ProfileController.index);
+
+module.exports = routes;
